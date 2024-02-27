c1c3745a9e5fdcbdb1a7d780e93c186693902a24
unknown <abraaodejesusximenes@gmail.com>
2024-02-27 14:25:54 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -150,7 +150,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
