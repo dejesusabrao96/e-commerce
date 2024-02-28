144218405ef399ec2f3d1221abe673ddd56d9fdf
unknown <abraaodejesusximenes@gmail.com>
2024-02-28 10:26:09 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -195,4 +195,4 @@ MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
 # teste teste tsetes
 # hello world
 # Activate Django-Heroku
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
