d8c1c3f75a4c452a584de87487f3b42315a11844
unknown <abraaodejesusximenes@gmail.com>
2024-02-29 16:50:33 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from django.core.management.utils import get_random_secret_key
 # import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
