5f48d0b9ad9e18b6acb3c3246bc5fa3247e5b6d5
unknown <abraaodejesusximenes@gmail.com>
2024-03-04 11:20:52 +0900

    First commit

--- store/models/customer.py
+++ store/models/customer.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=10)
-    last_name = models.CharField (max_length=10)
-    phone = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=6)
+    last_name = models.CharField (max_length=6)
+    phone = models.CharField(max_length=6)
     email=models.EmailField()
-    password = models.CharField(max_length=10)
+    password = models.CharField(max_length=6)
 
     #to save the data
     def register(self):
