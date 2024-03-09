ca22818cfc5f8f10ae2e41a6c2018359ff252da0
unknown <abraaodejesusximenes@gmail.com>
2024-03-09 10:35:43 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -196,11 +196,20 @@ LOGIN_URL = 'login'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STOREAGE = "whitenoise.storeage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STOREAGE = "whitenoise.storeage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
@@ -209,6 +218,9 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = '/app/media/'  # Adjust the path as needed
+MEDIA_URL = '/media/'
 
 # CKEditor Settings
 CKEDITOR_UPLOAD_PATH = 'uploads/'
