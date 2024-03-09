6789937735cad65218aca0819258164827a895a7
unknown <abraaodejesusximenes@gmail.com>
2024-03-09 10:21:16 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -206,9 +206,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-# MEDIA_URL = '/media/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/opt/render/project/src/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
