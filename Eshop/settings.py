3df275c842880557e08be7ad5d16f2bc20697ba1
unknown <abraaodejesusximenes@gmail.com>
2024-03-09 11:42:45 +0900

    First commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -215,9 +215,16 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # MEDIA_ROOT = '/app/media/'  # Adjust the path as needed
 # MEDIA_URL = '/media/'
